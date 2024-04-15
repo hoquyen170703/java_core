563c79d877dfab4ce5a96fed649dfb1478d5f651
hoquy <hoquyen1707@gmail.com>
2024-04-15 20:34:22 +0700

    btvn th1

---
+++ src/example/Address.java
@@ -0,0 +1,15 @@
+package example;
+
+import lombok.Data;
+
+@Data
+public class Address {
+
+    private String cityName;
+
+    private String districtName;
+
+    private String streetName;
+
+
+}
---
+++ src/example/Book.java
@@ -0,0 +1,24 @@
+package example;
+
+import lombok.Data;
+
+@Data
+public class Book {
+
+    private int book_id;
+
+    private String book_title;
+
+    private String book_author;
+
+    private String book_summary;
+
+    private String book_detail;
+
+    private short book_number_pages;
+
+    private String book_images;
+
+    private String book_notes;
+
+}
